715ab3b9f04bfe7f7aa9b0e8319f9d04e3f0b06b
Jaehwang Jung <tomtomjhj@gmail.com>
2022-04-01 19:53:45 +0900

    Add tests

--- test.v
+++ test.v
@@ -4,6 +4,22 @@ Inductive εμπτγ :=.
 
 Definition stringly_typed : string := ". Inductive nope :=. """.
 
+Inductive tree : Set :=
+  | node : forest -> tree
+with forest : Set :=
+  | emptyf : forest
+  | consf : tree -> forest -> forest.
+
+Fixpoint tree_size (t:tree) {struct t} : nat :=
+  match t with
+  | node f => S (forest_size f)
+  end
+with forest_size (f:forest) {struct f} : nat :=
+  match f with
+  | emptyf => 0
+  | consf t f' => (tree_size t + forest_size f')
+  end.
+
 Inductive inductive_with_match1 (a : match True with _ => True end) :=
   | inductive_with_match1_ctor0 : ((((True)))) -> inductive_with_match1 a
   | inductive_with_match1_ctor1 : match "with"%string with _ => True end -> inductive_with_match1 a
@@ -24,3 +40,7 @@ Record R := RCtor {
   R_b : string := "}. ";
   R_c (a : nat) : nat -> nat
 } (* aoiwejf *).
+
+Class singleton_class := method `{implicit : nat} : nat.
+(* If both record body and implicit argument are possible, Coq prefers record body.  *)
+Class not_singleton_class := not_method {not_implicit : nat} (* : nat (* syntax error *) *).
